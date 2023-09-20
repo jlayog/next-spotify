5ca7248822e085445d404e39c6f1bcb2f772a698
jlayog <35618625+jlayog@users.noreply.github.com>
2023-09-20 15:23:56 -0400

    Update page.tsx

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <div className="text-green-500">
-      Main Content
+    <div className="
+      text-green-500 
+      bg-neutral-900 
+      rounded-lg 
+      h-full 
+      w-full 
+      overflow-hidden 
+      overflow-y-auto
+    ">
+
     </div>
   )
 }
