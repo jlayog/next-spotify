0b847e1ab0ecdb3b76fc13b297ef3bed0e07fee6
jlayog <35618625+jlayog@users.noreply.github.com>
2023-09-25 12:40:27 -0400

    Added styles, components, functions
    Base layout complete. Moving to Authentication.

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,5 +1,8 @@
 import { twMerge } from "tailwind-merge";
 
+// Define BoxProps
+// Create Functional Component that accepts single prop (props)
+// use twMerge function to merge Tailwind CSS class names with custom class names provided in props
 interface BoxProps {
     children: React.ReactNode;
     className: string;
