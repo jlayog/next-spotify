5cb49368fa8c04b56e182e940299d21b44637981
jlayog <35618625+jlayog@users.noreply.github.com>
2023-10-04 14:42:50 -0400

    Added comments

--- providers/SupabaseProvider.tsx
+++ providers/SupabaseProvider.tsx
@@ -1,22 +1,30 @@
 "use client";
 
+//import the useState hook from react
 import { useState } from "react";
 
+//import the types from the database
 import { Database } from "@/types_db";
+//import the auth helpers from the supabase library
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+//import the session context provider from the supabase library
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
+//define the props for the component
 interface SupabaseProviderProps {
     children:React.ReactNode;
 };
 
+//define the component
 const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
     children 
 }) => {
+    //set the state for the client
     const [supabaseClient] = useState(() => 
         createClientComponentClient<Database>()
     );
 
+    //return the session context provider with the client
     return (
         <SessionContextProvider supabaseClient={supabaseClient}>
             {children}
@@ -24,4 +32,5 @@ const SupabaseProvider: React.FC<SupabaseProviderProps> = ({
     )
 }
 
+//export the component
 export default SupabaseProvider;
--- providers/UserProvider.tsx
+++ providers/UserProvider.tsx
@@ -1,12 +1,16 @@
 "use client";
 
+// Import the UserContextProvider from the hooks folder
 import { MyUserContextProvider } from "@/hooks/useUser";
 
+// Create an interface for the UserProviderProps
 interface UserProviderProps {
     children: React.ReactNode;
 };
 
+// Create a React functional component called UserProvider
 const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
+    // Return the UserContextProvider with the children passed in
     return (
         <MyUserContextProvider>
             {children}
@@ -14,4 +18,5 @@ const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     )
 }
 
+// Export the UserProvider component
 export default UserProvider;
