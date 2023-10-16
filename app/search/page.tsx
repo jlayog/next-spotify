73320c16165dda59cd9bb8f329f4f813f5402d44
jlayog <35618625+jlayog@users.noreply.github.com>
2023-10-16 08:58:01 -0400

    Added liked playlist feature
    created action to retrieve liked_songs from database. Added authentication to page for only authenticated users to access their own liked playlists.

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import SearchInput from "@/components/SearchInput";
 import SearchContent from "./components/SearchContent";
 
+// do not cache
 export const revalidate = 0;
 
 interface SearchProps {
